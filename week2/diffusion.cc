96dc7fde25be3269048b9537a3a4b4cd25b6d502
sanhidalgoo <shidalgoo1@eafit.edu.co>
2020-08-13 21:47:11 -0500

    assignament

--- week2/diffusion.cc
+++ week2/diffusion.cc
@@ -9,17 +9,28 @@ int diffusion(const int n_particles,
               const float alpha, 
               VSLStreamStatePtr rnStream) {
   int n_escaped=0;
-  for (int i = 0; i < n_particles; i++) {
-    float x = 0.0f;
-    for (int j = 0; j < n_steps; j++) {
-      float rn;
+  // Store the Distances of each particle -- Initialize to Zero 
+  float x[n_particles];
+  x[0:n_particles]=0.0f;
+  
+  // Store Random Numbers for each particle 
+  float rn[n_particles];
+  //Intel MKL function to generate random numbers
+  vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, n_particles, rn, -1.0, 1.0);
+ 
+  // Outer loop will be over n steps 
+  for (int i = 0; i < n_steps; i++) {
+
+    for (int j = 0; j < n_particles; j++) { 
       
-      //Intel MKL function to generate random numbers
-      vsRngUniform(VSL_RNG_METHOD_UNIFORM_STD, rnStream, 1, &rn, -1.0, 1.0);
-      
-      x += dist_func(alpha, rn); 
+      x[j] += dist_func(alpha, rn[j]);
+	  
+	  //if (x[j] > x_threshold) n_escaped++; 
     }
-    if (x > x_threshold) n_escaped++;
   }
+
+  for(int i = 0; i < n_particles; i++)
+	if(x[i] > x_threshold) n_escaped++;
+ 
   return n_escaped;
 }
